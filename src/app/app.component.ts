baa661bb9b7bc9f7af1c31880bb4d16f429ee69f
BETSSON\pejo1 <per.jonsson@betssongroup.com>
2017-04-10 09:50:22 +0200

    feat(init): bootstrap app

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hospital-app',
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+
+export class AppComponent {
+  name = 'Per';
+}
