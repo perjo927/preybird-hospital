6cb21f82354650f92dd3a8d271c69c2e4dc935cf
perjo927 <perjo927@student.liu.se>
2017-04-14 13:20:52 +0200

    feat(prototype): add more styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Hospitality Inc.';
+  isNavBar = false;
+  toggleNav() {
+    this.isNavBar = !this.isNavBar;
+  }
 }
