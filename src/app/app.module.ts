658d48ea129184a167fd1223b5e26fc8896d3a56
perjo927 <perjo927@student.liu.se>
2017-04-14 11:53:26 +0200

    feat(prototype): add more styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app.component';
 
 @NgModule({
@@ -8,4 +7,5 @@ import { AppComponent }  from './app.component';
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
