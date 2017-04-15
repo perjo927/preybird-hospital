18052ebdbebbd618ce83acccd1186c9d89126239
perjo927 <perjo927@student.liu.se>
2017-04-15 09:25:13 +0200

    feat(api): break out components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    SpinnerComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 
 export class AppModule { }
