52de426b4ef73f61c23010e2a28b8569d28707fd
perjo927 <perjo927@student.liu.se>
2017-04-17 17:18:46 +0200

    feat(api): add consultation details

--- src/app/components/consultation-page/consultation-page.component.ts
+++ src/app/components/consultation-page/consultation-page.component.ts
@@ -1,7 +1,12 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { ConsultationService } from '../../services/consultation.service';
+import { DoctorService } from '../../services/doctors.service';
+import { ImageService } from '../../services/image.service';
+import { MachineService } from '../../services/machines.service';
 import { PatientService } from '../../services/patient.service';
+import { RoomService } from '../../services/rooms.service';
 import { Consultation } from '../../models/consultation.model';
+import { PatientImage } from '../../models/image.model';
 import { RegisteredPatient } from '../../models/registered-patient.model';
 
 @Component({
@@ -14,18 +19,26 @@ import { RegisteredPatient } from '../../models/registered-patient.model';
 export class ConsultationPageComponent implements OnInit {
 
   consultations: Array<Consultation>;
+  consultationDetails = {
+    patientImage: 'http://placehold.it/100x100',
+    doctor: {},
+    room: {},
+    machine: {}
+  };
   patients: Array<RegisteredPatient>;
   patientConsultations: Array<any>;
   datesMap = {};
   dates: any;
-  expanded: false; // TODO
   selectedDate: string;
 
   constructor(
     private consultationService: ConsultationService,
+    private doctorService: DoctorService,
+    private imageService: ImageService,
+    private machineService: MachineService,
     private patientService: PatientService,
-  ) {
-  }
+    private roomService: RoomService,
+  ) { }
 
   ngOnInit(): void {
     this.fetchData().then(() => {
@@ -39,7 +52,33 @@ export class ConsultationPageComponent implements OnInit {
   async fetchData() {
     this.consultations = await this.getConsultations();
     this.patients = await this.getPatients();
+  }
 
+  async setDetails(consultation: Consultation, patient: RegisteredPatient) {
+    const imageUrl = await this.getImage(patient.imageId);
+    if (imageUrl) {
+      this.consultationDetails.patientImage = imageUrl;
+    }
+    const doctor = await this.getDoctor(consultation.doctorId)
+    if (doctor) {
+      const doctorImageUrl = await this.getImage(doctor.imageId);
+      console.log(doctorImageUrl)
+      if (doctorImageUrl) {
+        doctor.imageUrl = doctorImageUrl;
+      }
+      this.consultationDetails.doctor = doctor;
+    }
+    // ,
+    // room: this.getRoom(element.roomId)
+    // ,
+    // machine: this.getMachine(room.treatmentMachineId)
+    // console.log(url, this.consultationDetails)    
+
+  }
+
+  handleDetails(event: any): void {
+    this.setDetails(event.consultation, event.patient);
+    // this.consultationDetails = {};
   }
 
   getPatient(id: string): RegisteredPatient {
@@ -57,10 +96,32 @@ export class ConsultationPageComponent implements OnInit {
     return null;
   }
 
+  private async getDoctor(id: string): Promise<any> {
+    try {
+      const doctor = await this.doctorService.get<any>(id);
+      console.log(doctor);
+      return doctor;
+    } catch (err) {
+      console.error(`${err.status} ${err.statusText}`);
+    }
+    return null;
+  }
+
   private async getPatients(): Promise<RegisteredPatient[]> {
     try {
       const patients: Array<RegisteredPatient> = await this.patientService.getAll<RegisteredPatient>();
       return patients;
+    } catch (err) {
+      console.error(`${err.status} ${err.statusText}`);
+    }
+    return null;
+  }
+
+  private async getImage(id: string): Promise<string> {
+    try {
+      const image: PatientImage = await this.imageService.get<PatientImage>(id);
+      console.log(image.url);
+      return image.url;
     } catch (err) {
       console.error(`${err.status} ${err.statusText}`);
     }
@@ -73,11 +134,13 @@ export class ConsultationPageComponent implements OnInit {
     });
   }
 
-  private mapConsultations(consultations: Array<Consultation>): Array<any> {
-    return consultations.map((element) => {
+  private mapConsultations(consultations: Array<Consultation>) {//: Promise<any> {
+    return consultations.map((element: Consultation) => {
+      const patient = this.getPatient(element.patientId);
       return {
         consultation: element,
-        patient: this.getPatient(element.patientId)
+        patient: patient
+
       };
     });
   }
--- src/app/components/consultation/consultation.component.ts
+++ src/app/components/consultation/consultation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChange } from '@angular/core';
 import { Consultation } from '../../models/consultation.model';
 import { RegisteredPatient } from '../../models/registered-patient.model';
 
@@ -8,15 +8,41 @@ import { RegisteredPatient } from '../../models/registered-patient.model';
     styleUrls: ['./consultation.css']
 })
 
-export class ConsultationComponent {
+export class ConsultationComponent implements OnChanges {
 
     @Input() consultation: Consultation;
+    @Input() doctor: any;
     @Input() patient: RegisteredPatient;
-    // @Input() details?: any;
+    @Input() patientImage: any;
+    @Input() room: any;
+
+    @Output() requestDetails = new EventEmitter<any>();
 
     isExpanded: boolean = false;
 
+    changeLog: string[] = [];
+    ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+        let log: string[] = [];
+        for (let propName in changes) {
+            let changedProp = changes[propName];
+            let to = JSON.stringify(changedProp.currentValue);
+            if (changedProp.isFirstChange()) {
+                log.push(`Initial value of ${propName} set to ${to}`);
+            } else {
+                let from = JSON.stringify(changedProp.previousValue);
+                log.push(`${propName} changed from ${from} to ${to}`);
+            }
+        }
+        this.changeLog.push(log.join(', '));
+        console.log(this.changeLog)
+    }
+
     toggleExpand(): void {
+        this.requestDetails.emit({
+            consultation: this.consultation,
+            patient: this.patient
+        });
+
         this.isExpanded = !this.isExpanded;
     }
 }
